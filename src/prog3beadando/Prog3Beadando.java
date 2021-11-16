c0a765538c7dcdd7507de9360b362633ac276441
Szabo Kata <55539634+Migrantkitten@users.noreply.github.com>
2021-11-16 22:21:15 +0100

    Add files via upload
    first commit

---
+++ src/prog3beadando/Prog3Beadando.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog3beadando;
+
+/**
+ *
+ * @author kszab
+ */
+public class Prog3Beadando {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
