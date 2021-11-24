8bed80813f69e495140da93aaa8e5535c5c0bb7d
Szabo Kata <k.szabo9808@gmail.com>
2021-11-24 17:19:51 +0100

    Meh write
    meh

--- src/prog3beadando/UserView.java
+++ src/prog3beadando/UserView.java
@@ -86,7 +86,7 @@ public class UserView extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
+        // TODO add your meh handling code here:
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
